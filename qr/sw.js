a5ab94dc328b6bcef8acc16aec9b9332cec65490
daniel9ny-glitch <daniel9ny@gmail.com>
2026-06-26 14:38:23 +0900

    Add files via upload

---
+++ qr/sw.js
@@ -0,0 +1,39 @@
+// 늘사랑 QR생성 - 오프라인 캐시용 서비스 워커 (네트워크 우선 방식)
+const CACHE_NAME = 'neulsarang-qr-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// 네트워크 우선: 온라인이면 항상 최신 파일을 받아오고, 받으면 캐시도 갱신.
+// 오프라인일 때만 캐시에서 꺼내 씀.
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request).then((cached) => cached || caches.match('./index.html')))
+  );
+});
